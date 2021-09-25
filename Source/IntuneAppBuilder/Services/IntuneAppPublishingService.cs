c50670a910e1af426e507741d5ad6505c5bad9aa
Jeff <jeff@simeoncloud.com>
2021-09-25 14:02:31 -0400

    test timeout

--- Source/IntuneAppBuilder/Services/IntuneAppPublishingService.cs
+++ Source/IntuneAppBuilder/Services/IntuneAppPublishingService.cs
@@ -88,7 +88,7 @@ namespace IntuneAppBuilder.Services
             var blockCount = 0;
             var blockIds = new List<string>();
 
-            const int chunkSize = 25 * 1024 * 1024;
+            const int chunkSize = 1 * 1024 * 1024;
             package.Data.Seek(0, SeekOrigin.Begin);
             var lastBlockId = (Math.Ceiling((double)package.Data.Length / chunkSize) - 1).ToString("0000");
             var sw = Stopwatch.StartNew();
@@ -109,6 +109,8 @@ namespace IntuneAppBuilder.Services
                 {
                     await TryPutBlockAsync(contentFile, blockId, ms);
                 }
+
+                await Task.Delay(450000);
 
                 blockIds.Add(blockId);
             }
