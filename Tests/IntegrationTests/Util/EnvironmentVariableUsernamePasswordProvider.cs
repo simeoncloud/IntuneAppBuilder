3d0780ddd54187a4eb2ed9c1aac0043db2bdcaa4
alexshek <ashek@simeoncloud.com>
2023-07-17 20:10:21 -0700

    Undo formatting update

--- Tests/IntegrationTests/Util/EnvironmentVariableUsernamePasswordProvider.cs
+++ Tests/IntegrationTests/Util/EnvironmentVariableUsernamePasswordProvider.cs
@@ -9,28 +9,27 @@ using Microsoft.Graph.Authentication;
 using Microsoft.Kiota.Abstractions;
 using Microsoft.Kiota.Abstractions.Authentication;
 
-namespace IntuneAppBuilder.IntegrationTests.Util
+namespace IntuneAppBuilder.IntegrationTests.Util;
+
+/// <summary>
+///     Auth provider for Graph client that uses environment variables and resource owner flow.
+/// </summary>
+public sealed class EnvironmentVariableUsernamePasswordProvider : IAuthenticationProvider
 {
-    /// <summary>
-    ///     Auth provider for Graph client that uses environment variables and resource owner flow.
-    /// </summary>
-    public sealed class EnvironmentVariableUsernamePasswordProvider : IAuthenticationProvider
+    private readonly Lazy<TokenCredential> tokenCredential = new(() =>
     {
-        private readonly Lazy<TokenCredential> tokenCredential = new(() =>
-        {
-            new[] { "Username", "Password" }.Select(var => $"AadAuth:{var}").Select(Environment.GetEnvironmentVariable).Where(string.IsNullOrEmpty).ToList()
-                .ForEach(missingVar => throw new InvalidOperationException($"Environment variable {missingVar} is not specified."));
+        new[] { "Username", "Password" }.Select(var => $"AadAuth:{var}").Select(Environment.GetEnvironmentVariable).Where(string.IsNullOrEmpty).ToList()
+            .ForEach(missingVar => throw new InvalidOperationException($"Environment variable {missingVar} is not specified."));
 
-            var username = Environment.GetEnvironmentVariable("AadAuth:Username");
-            return new UsernamePasswordCredential(
-                username,
-                Environment.GetEnvironmentVariable("AadAuth:Password"),
-                username?.Split('@').Last(),
-                "14d82eec-204b-4c2f-b7e8-296a70dab67e"); // Microsoft Graph PowerShell well known client id
-        });
+        var username = Environment.GetEnvironmentVariable("AadAuth:Username");
+        return new UsernamePasswordCredential(
+            username,
+            Environment.GetEnvironmentVariable("AadAuth:Password"),
+            username?.Split('@').Last(),
+            "14d82eec-204b-4c2f-b7e8-296a70dab67e"); // Microsoft Graph PowerShell well known client id
+    });
 
-        private IAuthenticationProvider InnerProvider => new AzureIdentityAuthenticationProvider(tokenCredential.Value, scopes: "DeviceManagementApps.ReadWrite.All");
+    private IAuthenticationProvider InnerProvider => new AzureIdentityAuthenticationProvider(tokenCredential.Value, scopes: "DeviceManagementApps.ReadWrite.All");
 
-        public async Task AuthenticateRequestAsync(RequestInformation request, Dictionary<string, object> additionalAuthenticationContext = default, CancellationToken cancellationToken = default) => await InnerProvider.AuthenticateRequestAsync(request, additionalAuthenticationContext, cancellationToken);
-    }
+    public async Task AuthenticateRequestAsync(RequestInformation request, Dictionary<string, object> additionalAuthenticationContext = default, CancellationToken cancellationToken = default) => await InnerProvider.AuthenticateRequestAsync(request, additionalAuthenticationContext, cancellationToken);
 }
